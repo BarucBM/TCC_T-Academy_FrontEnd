84e11a34e9a441783dd9772f2bff20cbda054934
julia-montibeler <juliamaralbeler@gmail.com>
2024-09-19 23:59:06 -0300

    feat: implement current weather card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
 
 export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'currentweather',
+        component: WeatherCardComponent
     },
     {
         path: '',
