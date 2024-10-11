469c4b81fc1068a718b352fa14a0b53fcfd5aef0
baruc <baruc.bm@hotmail.com>
2024-10-11 18:35:09 -0300

    feat: adds the "Cart is empty" card

--- src/app/features/cart/pages/cart-page/cart-page.component.ts
+++ src/app/features/cart/pages/cart-page/cart-page.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Event } from '../../../../core/models/event.model';
+import { CardModule } from 'primeng/card';
+import { SharedModule } from '../../../../shared/modules/shared.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [],
+  imports: [CardModule, SharedModule, CommonModule],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.scss'
 })
-export class CartPageComponent {
-  tickets:Event[] = []
+export class CartPageComponent implements OnInit {
+  events!:Event[];
+
+  ngOnInit(): void {
+    
+  }
+
 }
