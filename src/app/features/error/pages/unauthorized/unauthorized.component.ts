e31521494c9195e52bd35753a99cc7b5183ed678
Leticia <leticiaborchardt123@gmail.com>
2024-10-14 15:23:54 -0300

    feat: styling error pages

--- src/app/features/error/pages/unauthorized/unauthorized.component.ts
+++ src/app/features/error/pages/unauthorized/unauthorized.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../../shared/modules/shared.module';
+import { RouterLink } from '@angular/router';
+import { SwitchThemeComponent } from '../../../../shared/components/switch-theme/switch-theme.component';
+import { LanguageSelectorComponent } from '../../../../shared/components/language-selector/language-selector.component';
 
 @Component({
   selector: 'app-unauthorized',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, RouterLink, SwitchThemeComponent, LanguageSelectorComponent],
   templateUrl: './unauthorized.component.html',
   styleUrl: './unauthorized.component.scss'
 })
