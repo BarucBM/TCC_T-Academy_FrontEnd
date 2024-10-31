a6916aa0db317fb0aedd0c64f6ae97c4e0820cd4
Leticia <leticiaborchardt123@gmail.com>
2024-10-30 22:39:14 -0300

    feat: basic home page

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { WeatherCardComponent } from '../../../../shared/components/weather-card/weather-card.component';
 import { UserCalendarComponent } from '../../../events/components/user-calendar/user-calendar.component';
 import { AuthService } from '../../../../core/auth/services/auth.service';
+import { CardModule } from 'primeng/card';
+import { SharedModule } from '../../../../shared/modules/shared.module';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WeatherCardComponent, UserCalendarComponent],
+  imports: [SharedModule, ButtonModule, WeatherCardComponent, UserCalendarComponent, CardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
